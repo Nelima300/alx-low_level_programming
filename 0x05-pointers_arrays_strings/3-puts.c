18bf62faf129f946b6a29a8475e4c8b72eddf16b
Nelima300 <ruthsichangi2002@gmail.com>
2022-09-19 16:54:08 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -7,13 +7,11 @@
  */
 void _puts(char *str)
 {
-int i = 0;
-
-while (str[i] != \0')
-{
+int i;
+for(i= o; str[i] ! = '\0'; i++)                                                                       
+{  
 _putchar(str[i]);
-i++;
-}
+} 
 _putchar('\n');
 }
 
